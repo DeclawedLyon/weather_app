10e97af61c911294f541c445cc3619f62b23576b
DeclawedLyon <declan.e.lyons@gmail.com>
2022-01-03 14:22:50 -0700

    initialized app using npx create-react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DayDisplay from './components/DayDisplay';
 
 function App() {
   return (
     <div className="App">
+      <DayDisplay/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
